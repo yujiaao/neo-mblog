28cc78595f53e9889464a6580bf8bbff41155ff4
邢万祥 <xingwanxiang@llzg.cn>
2016-01-29 17:20:31 +0800

    a working version

--- src/main/java/com/chengfu/neomblog/controller/PublicController.java
+++ src/main/java/com/chengfu/neomblog/controller/PublicController.java
@@ -3,10 +3,17 @@ package com.chengfu.neomblog.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.neo4j.helpers.collection.ClosableIterable;
+
+
+
+
+
+import org.neo4j.graphdb.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.neo4j.conversion.Result;
+import org.springframework.data.neo4j.core.GraphDatabase;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +22,8 @@ import com.chengfu.neomblog.model.User;
 import com.chengfu.neomblog.repository.UserRepository;
 import com.chengfu.neomblog.util.Utils;
 
+
+@Transactional
 @Controller
 public class PublicController {
 	@Autowired
@@ -23,14 +32,24 @@ public class PublicController {
 	@Autowired
 	Utils utils;
 	
+	@Autowired GraphDatabase graphDatabase;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String publicInfo(Model model) {
-		Result<User> users = userRepository.findAll();
-		List<User> result = new ArrayList<User>();
-		for (User user : users) {
-			result.add(user);
-		}
+		Transaction tx = graphDatabase.beginTx();
+		
+		try {
+		List<User> result = userRepository.findAll().as(List.class);
 		model.addAttribute("users", result);
+		tx.success();
+		} finally {
+			tx.close();
+		}
+//		List<User> result = new ArrayList<User>(); 
+//		for (User user : users) {
+//			result.add(user);
+//		}
+	
 		User currentUser = userRepository.findByLoginName(utils.getCurrentUsername());
 		model.addAttribute("currentUser", currentUser);
 		return "publicInfo";
